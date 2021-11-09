8c1fd4d105d9d9fcd1d1a4289d536b3d953b447e
Sameer Chhonkar <sameerchhonkar@Sameers-MacBook-Air.local>
2021-11-09 20:23:45 +0530

    Resume App

---
+++ lib/const.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// Text colors
+const cTextHeaderColor = Color(0xff2A2A2A);
+const cTextFeatureColor = Color(0xff707070);
+const cTextLabelColor = Colors.white;
+
+// Detail for profile item
+const cProfileTopic = 'About Me';
+const cProfileFeature = "I define myself through the these words: Sensible, Active,Mysterious,Energetic, Enthusiastic,and Responsible — a mix of emotions to accumulate and make a human being.";
+const cProfileLabel = 'Profile';
+const cProfilePicture = 'assets/images/avatar.png';
+const cProfileIconImage = 'assets/images/profile.png';
+const cProfileColor = Color(0xff845EC2);
+const cProfileBegColor = Color(0xff845EC2);
+const cProfileEndColor = Color(0xffB8A1DC);
+
+// Detail for education item
+const cEducationTopic = 'Educational Background';
+const cEducationFeature =
+    'B.Tech in\nElectrical And Electronics Engineering\n\nAKGEC Ghaziabad\n' + '2020 - 2024\nSGPA 8.45';
+const cEducationLabel = 'Education';
+const cEducationPicture = 'assets/images/book.png';
+const cEducationIconImage = 'assets/images/education.png';
+const cEducationColor = Color(0xffFFC75F);
+const cEduBegColor = Color(0xffFFC75F);
+const cEduEndColor = Color(0xffF3DDB2);
+
+// Detail for Technical Skills item
+const cExperienceTopic = 'Technical Skills';
+const cExperienceFeature = '- HTML\n' + ' - C/C++\n' + '- Python\n'+ '- Adobe Photoshop\n'+ '- Adobe Illustrator\n'+ '- Adobe After Effects\n';
+const cExperienceLabel = 'Skills';
+const cExperiencePicture = 'assets/images/laptop.png';
+const cExperienceIconImage = 'assets/images/experience.png';
+const cExperienceColor = Color(0xffFF5E78);
+const cExpBegColor = Color(0xffF8C8D0);
+const cExpEndColor = Color(0xffFF5E78);
+
+final cCaptionTextSize = 20.0.sp;
+const cStartingIndex = 0;
